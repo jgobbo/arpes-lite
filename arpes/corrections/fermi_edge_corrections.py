fixed kspace bounds calculation

arpes/corrections/fermi_edge_corrections.py
@@ -1,6 +1,5 @@
 """Automated utilities for calculating Fermi edge corrections."""
 
-from typing import Optional
 import lmfit as lf
 import matplotlib.pyplot as plt
 import numpy as np
@@ -253,7 +252,7 @@ def apply_photon_energy_fermi_edge_correction(
 
 
 def apply_quadratic_fermi_edge_correction(
-    arr: xr.DataArray, correction: Optional[lf.model.ModelResult] = None, offset=None
+    arr: xr.DataArray, correction: lf.model.ModelResult = None, offset=None
 ):
     """Applies a Fermi edge correction using a quadratic fit for the edge."""
     assert isinstance(arr, xr.DataArray)
@@ -314,28 +313,33 @@ def fit_fermi_edge(cut: xr.DataArray) -> xr.DataArray:
 def fix_fermi_edge(
     dataset: DataType,
     broadcast_fit: bool = True,
-    edge_fit: Optional[xr.DataArray] = None,
-    **kwargs,
+    edge_fit: xr.DataArray = None,
+    smoothing_sigma: float = 1.0,
 ) -> DataType:
-    """
-    Automatically corrects the Fermi edge in a dataset. If the measurement was done with
-    a curved slit, the Fermi edge will only be shifted to 0. If the measurement was done
-    with a straight slit, the Fermi edge will be shifted to 0 and the curved Fermi edge
-    will be corrected (`slit_shape` attr needs to be set to `straight`).
+    """Automatically correct the Fermi edge in a dataset.
 
-    Args:
-        ds: The dataset to correct
-        broadcast_fit: Whether to fit the Fermi edge of the summed spectrum and then
-            broadcast to all cuts or fit each cut individually.
-        edge_fit: A precomputed edge fit to use. Typically generated using
-            `fit_fermi_edge` on metal reference data.
+    If the measurement was done with a curved slit, the Fermi edge will only be shifted
+    to 0. If the measurement was done with a straight slit, the Fermi edge will be
+    shifted to 0 and the curved Fermi edge will be corrected (`slit_shape` attr needs to
+    be set to `straight`).
 
-    Kwargs:
-        smoothing_sigma: The sigma value for the Gaussian filter used to smooth the
-            integrated edc for fermi_edge detection. Increasing this can fix faulty
-            edge detection in noisy data.
+    Args
+    ----
+    dataset : DataType
+        The dataset to correct
+    broadcast_fit : bool
+        Whether to fit the Fermi edge of the summed spectrum and then broadcast to all
+        cuts or fit each cut individually.
+    edge_fit : DataArray
+        A precomputed edge fit to use. Typically generated using `fit_fermi_edge` on
+        metal reference data.
+    smoothing_sigma : float
+        The sigma value for the Gaussian filter used to smooth the integrated edc for
+        Fermi edge detection. Increasing this can fix faulty edge detection in noisy
+        data.
 
-    Returns:
+    Returns
+    -------
         A new dataset with the Fermi edge corrected.
     """
     HEMISPHERE_DIMS = {"eV", "phi"}
@@ -353,7 +357,7 @@ def fix_fermi_edge(
             original_spectrum.sum(
                 [dim for dim in original_spectrum.dims if dim != "eV"]
             ),
-            kwargs.get("smoothing_sigma", 1),
+            sigma=smoothing_sigma,
         )
         dropoff_index = np.argmin(np.diff(integrated_edc))
         dataset.coords["eV"] = (

arpes/utilities/conversion/bounds_calculations.py
@@ -3,10 +3,16 @@
 Mostly these are used as common helper routines to the coordinate conversion code,
 which is responsible for actually outputing the desired bounds.
 """
+
 import numpy as np
 
-import arpes.constants
+from arpes.constants import K_INV_ANGSTROM
 from arpes.typing import DataType
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import xarray as xr
 
 __all__ = (
     "calculate_kp_kz_bounds",
@@ -79,10 +85,8 @@ def full_angles_to_k(
 
     we may substitute cos^2(zeta) for 1 - sin^2(zeta) which is 1 - (vrchi_x **2 + vrchi_y ** 2) above.
     """
-    k_par = arpes.constants.K_INV_ANGSTROM * np.sqrt(kinetic_energy)
-    k_perp = arpes.constants.K_INV_ANGSTROM * np.sqrt(
-        kinetic_energy * (1 - v_par_sq) + inner_potential
-    )
+    k_par = K_INV_ANGSTROM * np.sqrt(kinetic_energy)
+    k_perp = K_INV_ANGSTROM * np.sqrt(kinetic_energy * (1 - v_par_sq) + inner_potential)
 
     return k_par * vrchi_x, k_par * vrchi_y, k_perp * vrchi_z
 
@@ -90,31 +94,22 @@ def full_angles_to_k(
 def euler_to_kx(kinetic_energy, phi, beta, theta=0, slit_is_vertical=False):
     """Calculates kx from the phi/beta Euler angles given the experimental geometry."""
     if slit_is_vertical:
-        return (
-            arpes.constants.K_INV_ANGSTROM
-            * np.sqrt(kinetic_energy)
-            * np.sin(beta)
-            * np.cos(phi)
-        )
+        return K_INV_ANGSTROM * np.sqrt(kinetic_energy) * np.sin(beta) * np.cos(phi)
     else:
-        return (
-            arpes.constants.K_INV_ANGSTROM
-            * np.sqrt(kinetic_energy)
-            * np.sin(phi + theta)
-        )
+        return K_INV_ANGSTROM * np.sqrt(kinetic_energy) * np.sin(phi + theta)
 
 
 def euler_to_ky(kinetic_energy, phi, beta, theta=0, slit_is_vertical=False):
     """Calculates ky from the phi/beta Euler angles given the experimental geometry."""
     if slit_is_vertical:
         return (
-            arpes.constants.K_INV_ANGSTROM
+            K_INV_ANGSTROM
             * np.sqrt(kinetic_energy)
             * (np.cos(theta) * np.sin(phi) + np.cos(beta) * np.cos(phi) * np.sin(theta))
         )
     else:
         return (
-            arpes.constants.K_INV_ANGSTROM
+            K_INV_ANGSTROM
             * np.sqrt(kinetic_energy)
             * (np.cos(phi + theta) * np.sin(beta),)
         )
@@ -132,31 +127,19 @@ def euler_to_kz(
     else:
         beta_term = np.cos(phi + theta) * np.cos(beta)
 
-    return arpes.constants.K_INV_ANGSTROM * np.sqrt(
-        kinetic_energy * beta_term**2 + inner_potential
-    )
+    return K_INV_ANGSTROM * np.sqrt(kinetic_energy * beta_term**2 + inner_potential)
 
 
 def spherical_to_kx(
     kinetic_energy: np.ndarray, theta: np.float64, phi: np.ndarray
 ) -> np.ndarray:
     """Calculates kx from the sample spherical (emission, not measurement) coordinates."""
-    return (
-        arpes.constants.K_INV_ANGSTROM
-        * np.sqrt(kinetic_energy)
-        * np.sin(theta)
-        * np.cos(phi)
-    )
+    return K_INV_ANGSTROM * np.sqrt(kinetic_energy) * np.sin(theta) * np.cos(phi)
 
 
 def spherical_to_ky(kinetic_energy, theta, phi):
     """Calculates ky from the sample spherical (emission, not measurement) coordinates."""
-    return (
-        arpes.constants.K_INV_ANGSTROM
-        * np.sqrt(kinetic_energy)
-        * np.sin(theta)
-        * np.sin(phi)
-    )
+    return K_INV_ANGSTROM * np.sqrt(kinetic_energy) * np.sin(theta) * np.sin(phi)
 
 
 def spherical_to_kz(
@@ -175,9 +158,7 @@ def spherical_to_kz(
     Returns:
         The out of plane momentum, kz.
     """
-    return arpes.constants.K_INV_ANGSTROM * np.sqrt(
-        kinetic_energy * np.cos(theta) ** 2 + inner_V
-    )
+    return K_INV_ANGSTROM * np.sqrt(kinetic_energy * np.cos(theta) ** 2 + inner_V)
 
 
 def calculate_kp_kz_bounds(data: DataType):
@@ -212,92 +193,64 @@ def calculate_kp_kz_bounds(data: DataType):
     )
 
 
-def calculate_kp_bounds(data: DataType):
+def calculate_kp_bounds(data: DataType, phi_offset: float, perpendicular_offset: float):
     """Calculates kp bounds for a single ARPES cut."""
-    phi_coords = data.coords["phi"].values - data.S.phi_offset
-    beta = float(data.coords["beta"]) - data.S.beta_offset
-
-    phi_low, phi_high = np.min(phi_coords), np.max(phi_coords)
-    phi_mid = (phi_high + phi_low) / 2
-
-    sampled_phi_values = np.array([phi_low, phi_mid, phi_high])
-
-    kinetic_energy = data.coords["eV"].values.max()
-    kps = (
-        arpes.constants.K_INV_ANGSTROM
-        * np.sqrt(kinetic_energy)
-        * np.sin(sampled_phi_values)
-        * np.cos(beta)
+    phi_coords = data.coords["phi"].values - phi_offset
+    phi_bounds = np.min(phi_coords), np.max(phi_coords)
+    kinetic_energy_sqrt = np.sqrt(
+        data.S.hv - data.S.work_function + data.coords["eV"].values.max()
     )
 
-    return round(np.min(kps), 2), round(np.max(kps), 2)
+    kp_bounds: np.ndarray = (
+        K_INV_ANGSTROM
+        * kinetic_energy_sqrt
+        * np.cos(perpendicular_offset)
+        * np.sin(phi_bounds)
+    )
+
+    return kp_bounds.round(2)
 
 
-def calculate_kx_ky_bounds(data: DataType):
-    """Calculates the kx and ky range for a dataset with a fixed photon energy.
+def calculate_kx_ky_bounds(
+    data: DataType, phi_offset: float, scan_offset: float, scan_axis: str = "beta"
+):
+    """Calculate the kx and ky range for a dataset with a fixed photon energy.
 
     This is used to infer the gridding that should be used for a k-space conversion.
     Based on Jonathan Denlinger's old codes
 
-    Args:
-        arr: Dataset that includes a key indicating the photon energy of
-          the scan
+    Args
+    ----
+    data : DataType
+        Dataset that includes a key indicating the photon energy of the scan
 
-    Returns:
+    Returns
+    -------
         ((kx_low, kx_high,), (ky_low, ky_high,))
     """
-    phi_coords, beta_coords = (
-        data.coords["phi"] - data.S.phi_offset,
-        data.coords["beta"] - data.S.beta_offset,
+    kinetic_energy_sqrt = np.sqrt(
+        data.S.hv - data.S.work_function + data.coords["eV"].values.max()
     )
 
-    # Sample hopefully representatively along the edges
-    phi_low, phi_high = np.min(phi_coords), np.max(phi_coords)
-    beta_low, beta_high = np.min(beta_coords), np.max(beta_coords)
-    phi_mid = (phi_high + phi_low) / 2
-    beta_mid = (beta_high + beta_low) / 2
+    phi_coords: "xr.DataArray" = data.S.lookup_coord("phi") - phi_offset
+    scan_coords: "xr.DataArray" = data.S.lookup_coord(scan_axis) - scan_offset
+    phi_checkpoints = [phi_coords.min(), phi_coords.mean(), phi_coords.max()]
+    scan_checkpoints = [scan_coords.min(), scan_coords.mean(), scan_coords.max()]
 
-    sampled_phi_values = np.array(
-        [
-            phi_high,
-            phi_high,
-            phi_mid,
-            phi_low,
-            phi_low,
-            phi_low,
-            phi_mid,
-            phi_high,
-            phi_high,
-        ]
-    )
-    sampled_beta_values = np.array(
-        [
-            beta_mid,
-            beta_high,
-            beta_high,
-            beta_high,
-            beta_mid,
-            beta_low,
-            beta_low,
-            beta_low,
-            beta_mid,
-        ]
-    )
-    kinetic_energy = data.coords["eV"].values.max()
-
-    kxs = (
-        arpes.constants.K_INV_ANGSTROM
-        * np.sqrt(kinetic_energy)
-        * np.sin(sampled_phi_values)
-    )
-    kys = (
-        arpes.constants.K_INV_ANGSTROM
-        * np.sqrt(kinetic_energy)
-        * np.cos(sampled_phi_values)
-        * np.sin(sampled_beta_values)
-    )
-
-    return (
-        (round(np.min(kxs), 2), round(np.max(kxs), 2)),
-        (round(np.min(kys), 2), round(np.max(kys), 2)),
+    potential_kx_bounds = []
+    potential_ky_bounds = []
+    for phi_checkpoint in phi_checkpoints:
+        potential_kx_bounds.append(
+            np.sin(phi_checkpoint) * kinetic_energy_sqrt * K_INV_ANGSTROM
+        )
+        for scan_checkpoint in scan_checkpoints:
+            potential_ky_bounds.append(
+                np.sin(scan_checkpoint)
+                * np.cos(phi_checkpoint)
+                * kinetic_energy_sqrt
+                * K_INV_ANGSTROM
+            )
+    return (min(potential_kx_bounds), max(potential_kx_bounds)), (
+        min(potential_ky_bounds),
+        max(potential_ky_bounds),
     )

arpes/utilities/conversion/kx_ky_conversion.py
@@ -79,26 +79,46 @@ def _safe_compute_k_tot(hv, work_function, binding_energy):
 class ConvertKp(CoordinateConverter):
     """A momentum converter for single ARPES (kp) cuts."""
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        """Initialize cached coordinates."""
-        super().__init__(*args, **kwargs)
-        self.k_tot = None
-        self.phi = None
+    k_tot: np.ndarray = None
+    phi: np.ndarray = None
+
+    def __init__(self, ds: xr.Dataset, *args: Any, **kwargs: Any) -> None:
+        """Initialize the kp momentum converter and cached coordinate values."""
+        super().__init__(ds, *args, **kwargs)
+
+        if self.is_slit_vertical:
+            self.parallel_angle = "beta"
+            self.perpendicular_angles = ["psi", "theta"]
+        else:
+            self.parallel_angle = "theta"
+            self.perpendicular_angles = ["psi", "beta"]
+
+    @property
+    def phi_offset(self) -> float:
+        """The offset for the phi angle."""
+        return self.ds.S.phi_offset + self.ds.S.lookup_offset_coord(self.parallel_angle)
+
+    @property
+    def perpendicular_offset(self) -> float:
+        """The offset for the perpendicular angle."""
+        return sum(
+            self.ds.S.lookup_offset_coord(angle) for angle in self.perpendicular_angles
+        )
 
     def get_coordinates(
         self, resolution: dict = None, bounds: dict = None
     ) -> dict[str, np.ndarray]:
         """Calculates appropriate coordinate bounds."""
-        if resolution is None:
-            resolution = {}
-        if bounds is None:
-            bounds = {}
+        resolution = {} if resolution is None else resolution
+        bounds = {} if bounds is None else bounds
 
         coordinates = super().get_coordinates(resolution, bounds=bounds)
-        (kp_low, kp_high) = calculate_kp_bounds(self.ds)
-
         if "kp" in bounds:
             kp_low, kp_high = bounds["kp"]
+        else:
+            kp_low, kp_high = calculate_kp_bounds(
+                self.ds, self.phi_offset, self.perpendicular_offset
+            )
 
         inferred_kp_res = (kp_high - kp_low + 2 * K_SPACE_BORDER) / len(
             self.ds.coords["phi"]
@@ -139,17 +159,6 @@ class ConvertKp(CoordinateConverter):
         if self.phi is not None:
             return self.phi
 
-        if self.is_slit_vertical:
-            polar_angle = self.ds.S.lookup_offset_coord(
-                "theta"
-            ) + self.ds.S.lookup_offset_coord("psi")
-            parallel_angle = self.ds.S.lookup_offset_coord("beta")
-        else:
-            polar_angle = self.ds.S.lookup_offset_coord(
-                "beta"
-            ) + self.ds.S.lookup_offset_coord("psi")
-            parallel_angle = self.ds.S.lookup_offset_coord("theta")
-
         if self.k_tot is None:
             self.compute_k_tot(binding_energy)
 
@@ -158,20 +167,18 @@ class ConvertKp(CoordinateConverter):
         assert len(self.k_tot) == len(kp) or len(self.k_tot) == 1
 
         _small_angle_arcsin(
-            kp / np.cos(polar_angle),
+            kp / np.cos(self.perpendicular_offset),
             self.k_tot,
             self.phi,
-            self.ds.S.phi_offset + parallel_angle,
+            self.phi_offset,
             par_tot,
             False,
         )
 
-        try:
+        if self.calibration is not None:
             self.phi = self.calibration.correct_detector_angle(
                 eV=binding_energy, phi=self.phi
             )
-        except:
-            pass
         return self.phi
 
     def conversion_for(self, dim: str) -> Callable:
@@ -192,86 +199,87 @@ class ConvertKxKy(CoordinateConverter):
     electrostatic deflector.
     """
 
+    k_tot: np.ndarray = None
+    phi: np.ndarray = None
+    perp_angle: np.ndarray = None
+    rkx: np.ndarray = None
+    rky: np.ndarray = None
+
     def __init__(self, ds: xr.Dataset, *args: list[str], **kwargs: Any) -> None:
         """Initialize the kx-ky momentum converter and cached coordinate values."""
         super().__init__(ds, *args, **kwargs)
-        self.k_tot = None
-        self.phi = None
-        # the angle perpendicular to phi as appropriate to the scan, this can be any of
-        # psi, theta, beta
-        self.perp_angle = None
-
-        self.rkx = None
-        self.rky = None
-
         # accept either vertical or horizontal, fail otherwise
         if not any(
             np.abs(ds.alpha - alpha_option) < (np.pi / 180)
             for alpha_option in [0, np.pi / 2]
         ):
             raise ValueError(
-                "You must convert either vertical or horizontal slit data with this converter."
+                "You must convert either vertical or horizontal slit data with this "
+                "converter."
             )
 
         self.direct_angles = (
             "phi",
             [d for d in ["psi", "beta", "theta"] if d in ds.indexes][0],
         )
+        assert len(self.direct_angles) == 2, "couldn't find an appropriate scan angle"
 
         if self.direct_angles[1] != "psi":
             # psi allows for either orientation
-            assert (self.direct_angles[1] in {"theta"}) != (not self.is_slit_vertical)
+            assert (
+                self.direct_angles[1] == "beta"
+            ) != self.is_slit_vertical, "slit orientation doesn't match scan angle"
 
-        # determine which other angles constitute equivalent sets
-        opposite_direct_angle = "theta" if "psi" in self.direct_angles else "psi"
         if self.is_slit_vertical:
             self.parallel_angles = (
                 "beta",
-                opposite_direct_angle,
+                "psi" if "theta" in self.direct_angles else "theta",
             )
         else:
             self.parallel_angles = (
                 "theta",
-                opposite_direct_angle,
+                "psi" if "beta" in self.direct_angles else "beta",
             )
+
+    @property
+    def phi_offset(self) -> float:
+        """The offset for the phi angle."""
+        return self.ds.S.phi_offset + self.ds.S.lookup_offset_coord(
+            self.parallel_angles[0]
+        )
+
+    @property
+    def perp_offset(self) -> float:
+        """The offset for the perpendicular angle."""
+        scan_angle = self.direct_angles[1]
+        return self.ds.S.lookup_offset(scan_angle) + self.ds.S.lookup_offset_coord(
+            self.parallel_angles[1]
+        )
 
     def get_coordinates(
         self, resolution: dict = None, bounds: dict = None
     ) -> dict[str, np.ndarray]:
         """Calculates appropriate coordinate bounds."""
-        if resolution is None:
-            resolution = {}
-        if bounds is None:
-            bounds = {}
+        resolution = {} if resolution is None else resolution
+        bounds = {} if bounds is None else bounds
 
         coordinates = super().get_coordinates(resolution, bounds=bounds)
 
-        ((kx_low, kx_high), (ky_low, ky_high)) = calculate_kx_ky_bounds(self.ds)
+        kx_angle, ky_angle = self.direct_angles
+        ((kx_low, kx_high), (ky_low, ky_high)) = calculate_kx_ky_bounds(
+            self.ds, self.phi_offset, self.perp_offset, ky_angle
+        )
 
         if "kx" in bounds:
             kx_low, kx_high = bounds["kx"]
         if "ky" in bounds:
             ky_low, ky_high = bounds["ky"]
 
-        kx_angle, ky_angle = self.direct_angles
-        if self.is_slit_vertical:
-            # phi actually measures along ky
-            ky_angle, kx_angle = kx_angle, ky_angle
-            ((ky_low, ky_high), (kx_low, kx_high)) = (
-                (kx_low, kx_high),
-                (ky_low, ky_high),
-            )
-
         len_ky_angle = len(self.ds.coords[ky_angle])
         len_kx_angle = len(self.ds.coords[kx_angle])
 
-        inferred_kx_res = (kx_high - kx_low + 2 * K_SPACE_BORDER) / len(
-            self.ds.coords[kx_angle]
-        )
-        inferred_ky_res = (ky_high - ky_low + 2 * K_SPACE_BORDER) / len(
-            self.ds.coords[ky_angle]
-        )
-
+        inferred_kx_res = (kx_high - kx_low + 2 * K_SPACE_BORDER) / len_kx_angle
+        inferred_ky_res = (ky_high - ky_low + 2 * K_SPACE_BORDER) / len_ky_angle
         # upsample a bit if there aren't that many points along a certain axis
         try:
             inferred_kx_res = [b for b in MOMENTUM_BREAKPOINTS if b < inferred_kx_res][
@@ -353,7 +361,7 @@ class ConvertKxKy(CoordinateConverter):
         kx: np.ndarray,
         ky: np.ndarray,
         *args: Any,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> np.ndarray:
         """Converts from momentum back to the analyzer angular axis."""
         if self.phi is not None:
@@ -368,37 +376,17 @@ class ConvertKxKy(CoordinateConverter):
         # This can be condensed but it is actually better not to condense it:
         # In this format, we can very easily compare to the raw coordinate conversion functions that
         # come from Mathematica in order to adjust signs, etc.
-        scan_angle = self.direct_angles[1]
         self.phi = np.zeros_like(ky)
-        offset = self.ds.S.phi_offset + self.ds.S.lookup_offset_coord(
-            self.parallel_angles[0]
-        )
 
         par_tot = isinstance(self.k_tot, np.ndarray) and len(self.k_tot) != 1
         assert len(self.k_tot) == len(self.phi) or len(self.k_tot) == 1
 
-        if scan_angle == "psi":
-            if self.is_slit_vertical:
-                _exact_arcsin(ky, kx, self.k_tot, self.phi, offset, par_tot, False)
-            else:
-                _exact_arcsin(kx, ky, self.k_tot, self.phi, offset, par_tot, False)
-        elif scan_angle == "beta":
-            # vertical slit
-            _small_angle_arcsin(kx, self.k_tot, self.phi, offset, par_tot, False)
-        elif scan_angle == "theta":
-            # vertical slit
-            _small_angle_arcsin(ky, self.k_tot, self.phi, offset, par_tot, False)
-        else:
-            raise ValueError(
-                "No recognized scan angle found for {}".format(self.parallel_angles[1])
-            )
+        _exact_arcsin(kx, ky, self.k_tot, self.phi, self.phi_offset, par_tot, False)
 
-        try:
+        if self.calibration is not None:
             self.phi = self.calibration.correct_detector_angle(
                 eV=binding_energy, phi=self.phi
             )
-        except:
-            pass
 
         return self.phi
 
@@ -408,9 +396,9 @@ class ConvertKxKy(CoordinateConverter):
         kx: np.ndarray,
         ky: np.ndarray,
         *args: Any,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> np.ndarray:
-        """Converts from momentum back to the scan angle perpendicular to the analyzer."""
+        """Convert momentum to the scan angle perpendicular to the analyzer slit."""
         if self.perp_angle is not None:
             return self.perp_angle
 
@@ -420,40 +408,13 @@ class ConvertKxKy(CoordinateConverter):
         if self.needs_rotation:
             kx, ky = self.rkx_rky(kx, ky)
 
-        scan_angle = self.direct_angles[1]
         self.perp_angle = np.zeros_like(kx)
 
         par_tot = isinstance(self.k_tot, np.ndarray) and len(self.k_tot) != 1
         assert len(self.k_tot) == len(self.perp_angle) or len(self.k_tot) == 1
 
-        if scan_angle == "psi":
-            if self.is_slit_vertical:
-                offset = self.ds.S.psi_offset - self.ds.S.lookup_offset_coord(
-                    self.parallel_angles[1]
-                )
-                _small_angle_arcsin(
-                    kx, self.k_tot, self.perp_angle, offset, par_tot, True
-                )
-            else:
-                offset = self.ds.S.psi_offset + self.ds.S.lookup_offset_coord(
-                    self.parallel_angles[1]
-                )
-                _small_angle_arcsin(
-                    ky, self.k_tot, self.perp_angle, offset, par_tot, False
-                )
-        elif scan_angle == "beta":
-            offset = self.ds.S.beta_offset + self.ds.S.lookup_offset_coord(
-                self.parallel_angles[1]
-            )
-            _exact_arcsin(ky, kx, self.k_tot, self.perp_angle, offset, par_tot, True)
-        elif scan_angle == "theta":
-            offset = self.ds.S.theta_offset - self.ds.S.lookup_offset_coord(
-                self.parallel_angles[1]
-            )
-            _exact_arcsin(kx, ky, self.k_tot, self.perp_angle, offset, par_tot, True)
-        else:
-            raise ValueError(
-                "No recognized scan angle found for {}".format(self.parallel_angles[1])
-            )
+        _exact_arcsin(
+            ky, kx, self.k_tot, self.perp_angle, self.perp_offset, par_tot, False
+        )
 
         return self.perp_angle